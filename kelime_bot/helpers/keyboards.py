3d11726dbc709824628db6fab4348dd6a5eee867
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-01-16 19:56:24 +0400

    Delete kelime_bot/helpers directory

--- kelime_bot/helpers/keyboards.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("🔮 Əyləncə Qrupu 🔮" , url= "t.me/cenublar")]
-])
-
-
