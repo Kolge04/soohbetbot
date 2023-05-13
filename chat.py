314054ce07b1c180a28b57216f3360fdcb60bdf9
REDOL <117319718+Kolge04@users.noreply.github.com>
2023-05-13 23:22:35 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -26,7 +26,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 Bot = Client(
     "Info Bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
+    api_id=int(os.environ.get("APP_ID")),
     api_hash=os.environ.get("API_HASH")
 )
 
